103230d6685d0f8e524e61fbcc8910fe20884049
Siddhartha Gudipati <me@websiddu.com>
2015-09-18 20:22:55 -0700

    Adding user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @guides = Guide.last(5).reverse
+    @guides = [] #Guide.last(5).reverse
     @events = Event.last(5).reverse
     @helps = Help.last(5).reverse
+
+
   end
 end
