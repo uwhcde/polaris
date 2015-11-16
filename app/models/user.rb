5714b1cc9c920d8e5cebc36b8fe573e0f163e5e7
Ankur Agrawal <ankuragrawal@ymail.com>
2015-11-16 11:12:54 -0800

    Updating devise gem's user registration
    Devise gem's user registration updated for the new fields in user table.
    Header is fixed to show user's name and a link to his profile. Paperclip
    is used to upload the picture.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :picture, styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url => "/images/:style/missing.jpg"
+  validates_attachment_content_type :picture, :content_type => [/\Aimage/, 'application/octet-stream']
+  # Validate filename
+  validates_attachment_file_name :picture, matches: [/png\Z/, /jpe?g\Z/]
+  # Explicitly do not validate
+  do_not_validate_attachment_file_type :picture
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
     resources :comments, :only => [:create]
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
+  resources :users, only: [:show]
+  
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
