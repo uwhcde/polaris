4c68002c96b01e4a7e95d01ae9d85ac117aa7621
Ankur Agrawal <ankuragrawal@ymail.com>
2015-11-15 22:05:21 -0800

    Updating schemas and migration files
    Updated schemas to remove steps table and migration file to fix the
    default values for upVotes and downVotes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151110092409) do
-  
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "adminpack"
-
-  create_table "bootsy_image_galleries", force: :cascade do |t|
-    t.integer  "bootsy_resource_id"
-    t.string   "bootsy_resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "bootsy_images", force: :cascade do |t|
-    t.string   "image_file"
-    t.integer  "image_gallery_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -60,14 +44,14 @@ ActiveRecord::Schema.define(version: 20151110092409) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "short_description"
-    t.integer  "user_id",           null: false
+    t.integer  "user_id",                       null: false
     t.text     "tagging_details"
     t.time     "publish_date"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "postType"
-    t.integer  "upVotes"
-    t.integer  "downVotes"
+    t.integer  "upVotes",           default: 0
+    t.integer  "downVotes",         default: 0
   end
 
   create_table "sections", force: :cascade do |t|
@@ -78,14 +62,6 @@ ActiveRecord::Schema.define(version: 20151110092409) do
     t.datetime "updated_at",  null: false
     t.integer  "post_id",     null: false
     t.integer  "user_id",     null: false
-  end
-
-  create_table "steps", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "position"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
