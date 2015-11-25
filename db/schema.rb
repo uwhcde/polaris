460976ecbbc63fa0697276e348d2cb4ab6a4de7b
Vishwas Shetty <vishshetty1504@gmail.com>
2015-11-24 16:37:54 -0800

    updating schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -16,20 +16,6 @@ ActiveRecord::Schema.define(version: 20151116063914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bootsy_image_galleries", force: :cascade do |t|
-    t.integer  "bootsy_resource_id"
-    t.string   "bootsy_resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "bootsy_images", force: :cascade do |t|
-    t.string   "image_file"
-    t.integer  "image_gallery_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
     t.string   "data_content_type"
@@ -76,14 +62,6 @@ ActiveRecord::Schema.define(version: 20151116063914) do
     t.datetime "updated_at",  null: false
     t.integer  "post_id",     null: false
     t.integer  "user_id",     null: false
-  end
-
-  create_table "steps", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "position"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
