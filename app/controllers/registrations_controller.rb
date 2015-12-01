f13c6ba17566caff0015bfe50882ac2955f7a67b
Ankur Agrawal <ankuragrawal@ymail.com>
2015-11-30 20:39:13 -0800

    Fix for user redirection after edit and for panel code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
   private
 
   def sign_up_params
@@ -7,6 +17,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:cover_image, :picture, :first_name, :last_name, :email, :password, :password_confirmation, :current_password, :description, :phone)
+    params.require(:user).permit(:cover_image, :picture, :first_name, :last_name, :email, :password, :password_confirmation, :description, :phone)
   end
 end
