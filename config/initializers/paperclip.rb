ce7597c6c65e02fd1eb1d1c3dadf07ac2e96f755
Siddhartha Gudipati <me@websiddu.com>
2016-01-31 23:28:51 -0800

    Fixing the assets

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,12 @@
 Paperclip.options[:content_type_mappings] = { jpeg: 'image/jpeg', jpg: "image/jpeg" }
+
+
+# config/initializers/paperclip.rb
+Paperclip.interpolates(:user_avatar) do |attachment, style|
+  ActionController::Base.helpers.asset_path("users/avatar/missing_#{style}.jpg")
+end
+
+Paperclip.interpolates(:user_cover) do |attachment, style|
+  ActionController::Base.helpers.asset_path("users/cover/missing_#{style}.jpg")
+end
+
