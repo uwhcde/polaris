9054f3f2f05abef47c589aac8c28fa7324a646d6
Siddhartha Gudipati <me@websiddu.com>
2016-03-10 13:44:37 -0800

    Fixing my cards

--- app/controllers/mycards_controller.rb
+++ app/controllers/mycards_controller.rb
@@ -4,11 +4,56 @@ class MycardsController < ApplicationController
 
 
   def index
-    @guides = Guide.evaluated_by(:bookmark, current_user)
-    @events = Event.evaluated_by(:bookmark, current_user)
-    @helps = Help.evaluated_by(:bookmark, current_user)
+    typeParam = params[:posttype].try(:downcase)
+    if !typeParam.present?
+        typeParam = 'all'
+    end
 
-    @posts = @guides + @events + @helps
+    sortParam = params[:sort].try(:downcase)
+    if !sortParam.present?
+        sortParam = 'recent'
+    end
+
+    sort = :updated_at
+    case sortParam
+    when 'recent'
+        sort = :updated_at
+    when 'popularity'
+        sort = :view_count
+    end
+
+    category = params[:c].try(:downcase)
+
+    if category.present?
+      case category
+      when 'all'
+          @posts = Guide.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Event.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Help.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Event.evaluated_by(:rsvp, current_user).order(sort => :asc).reverse
+          @posts = @posts.sort_by{|e| e[sort]}.reverse
+      when 'rsvp'
+          @posts = Event.evaluated_by(:rsvp, current_user).order(sort => :asc).reverse
+      when 'helps'
+          @posts = Help.tagged_with(category).order(sort => :asc).reverse
+      end
+    else
+      @posts = Guide.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Event.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Help.evaluated_by(:bookmark, current_user).order(sort => :asc).reverse +
+              Event.evaluated_by(:rsvp, current_user).order(sort => :asc).reverse
+          @posts = @posts.sort_by{|e| e[sort]}.reverse
+    end
+
+    @params = {"posttype" => params[:posttype],
+      "sort" => params[:sort],
+      "c" => params[:c]}
+
+    if !@posts.nil?
+      @posts = @posts.paginate(:page => params[:page], :per_page => 8)
+    end
+
+
   end
 
 end
