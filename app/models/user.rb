5b764f760487d317e3fdf8e5c4b3155155cc8eba
Siddhartha Gudipati <me@websiddu.com>
2016-03-12 22:17:12 -0800

    Adding and fixing the cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,10 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :helps
 
-  has_attached_file :cover_image, styles: { medium: "400x150", large: "800x300" }, :default_url => ':user_cover'
+  has_attached_file :cover_image,
+    styles: { medium: "400x150", large: "800x300" },
+    url: "/uploads/profile/cover/:id/:style_:basename.:extension",
+    default_url: ':user_cover'
 
   validates_attachment_content_type :cover_image, :content_type => [/\Aimage/, 'application/octet-stream']
   # Validate filename
@@ -17,7 +20,11 @@ class User < ActiveRecord::Base
   # Explicitly do not validate
   do_not_validate_attachment_file_type :cover_image
 
-  has_attached_file :picture, styles: { small: "64x64", medium: "100x100", large: "200x200" }, :default_url => ':user_avatar'
+  has_attached_file :picture,
+    styles: { small: "64x64", medium: "100x100", large: "200x200" },
+    default_url: ':user_avatar',
+    url: "/uploads/profile/avatar/:id/:style_:basename.:extension"
+
   validates_attachment_content_type :picture, :content_type => [/\Aimage/, 'application/octet-stream']
   # Validate filename
   validates_attachment_file_name :picture, matches: [/png\Z/, /jpe?g\Z/]
